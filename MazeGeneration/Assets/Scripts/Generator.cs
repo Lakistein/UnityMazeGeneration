9d410528b22f542c248e707cab2a43f3ba0aa190
Lazar Lazarevic <lazarlazarevic@msn.com>
2017-04-22 00:14:12 +0200

    Initial commit for Maze generation algorithms visualized in Unity

---
+++ MazeGeneration/Assets/Scripts/Generator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject Cell;
+    public int Size;
+    private GameObject cell;
+    void Start()
+    {
+        //MazeGeneratorDepthFirst.Cell[,] cells = MazeGeneratorDepthFirst.GenerateMaze(Size);
+        //var a = cells;
+
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    for (int j = 0; j < Size; j++)
+        //    {
+        MazeGeneratorDepthFirst.Cell currCell = MazeGeneratorDepthFirst.GenerateMaze(Size);
+        cell = GameObject.Instantiate(Cell);
+        cell.name = currCell.X + "," + currCell.Y;
+        cell.transform.position = new Vector3(currCell.X * 10, 0, currCell.Y * -10);
+        cell.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.red;
+        for (int w = 0; w < 4; w++)
+        {
+            if (currCell.Walls[w] == 1)
+            {
+                cell.transform.GetChild(w + 1).gameObject.SetActive(false);
+            }
+        }
+        //    }
+        //}
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            while (GenerateOne()) ;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GenerateOne();
+        }
+    }
+
+    bool GenerateOne()
+    {
+        cell.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+        MazeGeneratorDepthFirst.Cell c = MazeGeneratorDepthFirst.GenerateNext();
+        if (c == null) return false;
+        var a = GameObject.Find(c.X + "," + c.Y);
+        if (a == null)
+        {
+            GameObject celll = GameObject.Instantiate(Cell);
+            cell = celll;
+            cell.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+            celll.name = c.X + "," + c.Y;
+            celll.transform.position = new Vector3(c.X * 10, 0, c.Y * -10);
+            for (int w = 0; w < 4; w++)
+            {
+                if (c.Walls[w] == 1)
+                {
+                    celll.transform.GetChild(w + 1).gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            cell = a;
+            cell.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.red;
+            for (int w = 0; w < 4; w++)
+            {
+                if (c.Walls[w] == 1)
+                {
+                    cell.transform.GetChild(w + 1).gameObject.SetActive(false);
+                }
+            }
+        }
+        return true;
+    }
+
+    void GenerateAll()
+    {
+
+    }
+}
---
+++ MazeGeneration/Assets/Scripts/MazeGeneratorDepthFirst.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class MazeGeneratorDepthFirst
+{
+
+    public static Cell[,] _maze;
+    private static Random r = new Random(Guid.NewGuid().GetHashCode());
+
+    private static int _size;
+    private static Stack<Cell> _stack = new Stack<Cell>();
+    private static int visitedCells = 0;
+    static Cell curCell;
+
+    public static Cell GenerateMaze(int size)
+    {
+        _size = size;
+        _maze = new Cell[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                _maze[i, j] = new Cell(i, j);
+            }
+        }
+
+
+        _maze[0, 0] = new Cell(0, 0);
+        curCell = _maze[0, 0];
+        curCell.IsVisited = true;
+
+        //while(visitedCells < _size)
+        //{
+        //    var ne = GetUnvisitedNeighbours(curCell);
+
+        //    if(ne.Count > 0)
+        //    {
+        //        _stack.Push(curCell);
+        //        Cell chosen = ne[r.Next(0, ne.Count)];
+        //        chosen.Walls[GetDirection(curCell, chosen)] = 1;
+        //        chosen.IsVisited = true;
+        //        curCell = chosen;
+        //        visitedCells++;
+        //    }
+        //    else
+        //    {
+        //        if(_stack.Count > 0)
+        //        {
+        //            curCell = _stack.Pop();
+        //        }
+        //    }
+        //}
+
+        return curCell;
+    }
+
+    public static Cell GenerateNext()
+    {
+        if (visitedCells <= _size * _size)
+        {
+            var ne = GetUnvisitedNeighbours(curCell);
+
+            if (ne.Count > 0)
+            {
+                _stack.Push(curCell);
+                Cell chosen = ne[r.Next(0, ne.Count)];
+                chosen.Walls[GetDirection(curCell, chosen)] = 1;
+                curCell.Walls[GetDirection(chosen, curCell)] = 1;
+                chosen.IsVisited = true;
+                curCell = chosen;
+                visitedCells++;
+                return curCell;
+            }
+            else
+            {
+                if (_stack.Count > 0)
+                {
+                    curCell = _stack.Pop();
+                    return curCell;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static int GetDirection(Cell currCell, Cell newCell)
+    {
+        if (newCell.X < currCell.X) return 2;
+        if (newCell.Y < currCell.Y) return 3;
+        if (newCell.X > currCell.X) return 0;
+
+        return 1;
+    }
+
+    private static List<Cell> GetUnvisitedNeighbours(Cell c)
+    {
+        List<Cell> unvisited = new List<Cell>();
+        for (int i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    if (c.X > 0 && !_maze[c.X - 1, c.Y].IsVisited) unvisited.Add(_maze[c.X - 1, c.Y]);
+                    break;
+                case 1:
+                    if (c.Y > 0 && !_maze[c.X, c.Y - 1].IsVisited) unvisited.Add(_maze[c.X, c.Y - 1]);
+                    break;
+                case 2:
+                    if (c.X < _size - 1 && !_maze[c.X + 1, c.Y].IsVisited) unvisited.Add(_maze[c.X + 1, c.Y]);
+                    break;
+                case 3:
+                    if (c.Y < _size - 1 && !_maze[c.X, c.Y + 1].IsVisited) unvisited.Add(_maze[c.X, c.Y + 1]);
+                    break;
+            }
+        }
+        return unvisited;
+    }
+
+
+    public class Cell
+    {
+        /*
+         * 0 left
+         * 1 up
+         * 2 right
+         * 3 down
+         */
+        public int[] Walls = new int[4];
+        public bool IsVisited = false;
+        public int X, Y;
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
